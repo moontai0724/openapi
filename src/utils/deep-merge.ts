da6a9973537af6a2268137981e563d88626ca1d2
moontai0724 <moontai0724@gmail.com>
2024-04-30 13:30:32 +0800

    fix(type): set deepMerge function type
    don't know why the original deepMerge type will resulted in memory leak

--- src/utils/deep-merge.ts
+++ src/utils/deep-merge.ts
@@ -1,3 +1,6 @@
 import deepmerge from "@fastify/deepmerge";
 
-export const deepMerge = deepmerge({ all: true });
+export const deepMerge: <T extends unknown[]>(...args: T) => T[number] =
+  deepmerge({
+    all: true,
+  });
